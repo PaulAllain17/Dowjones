73c92ea2dc9aeb16da18549a2a468e5315490172
PaulAllain17 <paul.allain17@gmail.com>
2024-08-03 16:49:43 +0200

    Test repositories

---
+++ DowjonesAPIUnitTests/Repositories/CompanyRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using DowjonesAPI.Data;
+using DowjonesAPI.Models;
+using DowjonesAPI.Repositories;
+using Moq;
+
+namespace DowjonesAPIUnitTests.Repositories
+{
+	public class CompanyRepositoryTests
+	{
+		private Mock<IMockedDatabase> _mockedDatabase;
+
+		private CompanyRepository _companyRepository;
+
+		private List<Company> _companies;
+		private Company _company;
+
+		[SetUp]
+		public void Setup()
+		{
+			_mockedDatabase = new Mock<IMockedDatabase>();
+			_companies = [new Company { Name = "Apple" }, new Company { Name = "Microsoft" }];
+			_company = new Company { Name = "Apple", Id = 1 };
+
+			_mockedDatabase.Setup(m => m.GetCompany(1)).ReturnsAsync(_company);
+			_mockedDatabase.Setup(m => m.GetCompanies()).ReturnsAsync(_companies);
+
+			_companyRepository = new CompanyRepository(_mockedDatabase.Object);
+		}
+
+		[Test]
+		public void AddCompany_CallDatabase()
+		{
+			_companyRepository.AddCompany(_company);
+
+			_mockedDatabase.Verify(m => m.AddCompany(_company), Times.Once());
+		}
+
+		[Test]
+		public async Task GetCompanies_CallDatabase()
+		{
+			var result = await _companyRepository.GetCompanies();
+
+			_mockedDatabase.Verify(m => m.GetCompanies(), Times.Once());
+			Assert.That(result, Is.SameAs(_companies));
+		}
+
+		[Test]
+		public async Task GetCompany_IdProvided_CallService()
+		{
+			var result = await _companyRepository.GetCompany(1);
+
+			_mockedDatabase.Verify(m => m.GetCompany(1), Times.Once());
+			Assert.That(result, Is.SameAs(_company));
+		}
+
+		[Test]
+		public void RemoveCompany_CompanyProvided_CallService()
+		{
+			_companyRepository.RemoveCompany(_company);
+
+			_mockedDatabase.Verify(m => m.RemoveCompany(_company), Times.Once());
+		}
+
+		[Test]
+		public void UpdateCompany_CompanyProvided_CallService()
+		{
+			_companyRepository.UpdateCompany(_company);
+
+			_mockedDatabase.Verify(m => m.UpdateCompany(_company), Times.Once());
+		}
+
+		[Test]
+		public async Task CompanyExists_CompanyDoesExist_ReturnTrue()
+		{
+			var result = await _companyRepository.CompanyExists(1);
+
+			_mockedDatabase.Verify(m => m.GetCompany(1), Times.Once());
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public async Task CompanyExists_CompanyDoesNotExist_ReturnFalse()
+		{
+			var result = await _companyRepository.CompanyExists(2);
+
+			_mockedDatabase.Verify(m => m.GetCompany(2), Times.Once());
+			Assert.IsFalse(result);
+		}
+	}
+}
---
+++ DowjonesAPIUnitTests/Repositories/PersonRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using DowjonesAPI.Data;
+using DowjonesAPI.Models;
+using DowjonesAPI.Repositories;
+using Moq;
+
+namespace DowjonesAPIUnitTests.Repositories
+{
+	public class PersonRepositoryTests
+	{
+		private Mock<IMockedDatabase> _mockedDatabase;
+
+		private PersonRepository _personRepository;
+
+		private List<Person> _people;
+		private Person _person;
+
+		[SetUp]
+		public void Setup()
+		{
+			_mockedDatabase = new Mock<IMockedDatabase>();
+			_people = [new Person { Name = "Apple" }, new Person { Name = "Microsoft" }];
+			_person = new Person { Name = "Apple", Id = 1 };
+
+			_mockedDatabase.Setup(m => m.GetPerson(1)).ReturnsAsync(_person);
+			_mockedDatabase.Setup(m => m.GetPeople()).ReturnsAsync(_people);
+
+			_personRepository = new PersonRepository(_mockedDatabase.Object);
+		}
+
+		[Test]
+		public void AddPerson_CallDatabase()
+		{
+			_personRepository.AddPerson(_person);
+
+			_mockedDatabase.Verify(m => m.AddPerson(_person), Times.Once());
+		}
+
+		[Test]
+		public async Task GetPeople_CallDatabase()
+		{
+			var result = await _personRepository.GetPeople();
+
+			_mockedDatabase.Verify(m => m.GetPeople(), Times.Once());
+			Assert.That(result, Is.SameAs(_people));
+		}
+
+		[Test]
+		public async Task GetPerson_IdProvided_CallService()
+		{
+			var result = await _personRepository.GetPerson(1);
+
+			_mockedDatabase.Verify(m => m.GetPerson(1), Times.Once());
+			Assert.That(result, Is.SameAs(_person));
+		}
+
+		[Test]
+		public void RemovePerson_PersonProvided_CallService()
+		{
+			_personRepository.RemovePerson(_person);
+
+			_mockedDatabase.Verify(m => m.RemovePerson(_person), Times.Once());
+		}
+
+		[Test]
+		public void UpdatePerson_PersonProvided_CallService()
+		{
+			_personRepository.UpdatePerson(_person);
+
+			_mockedDatabase.Verify(m => m.UpdatePerson(_person), Times.Once());
+		}
+
+		[Test]
+		public async Task PersonExists_PersonDoesExist_ReturnTrue()
+		{
+			var result = await _personRepository.PersonExists(1);
+
+			_mockedDatabase.Verify(m => m.GetPerson(1), Times.Once());
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public async Task PersonExists_PersonDoesNotExist_ReturnFalse()
+		{
+			var result = await _personRepository.PersonExists(2);
+
+			_mockedDatabase.Verify(m => m.GetPerson(2), Times.Once());
+			Assert.IsFalse(result);
+		}
+	}
+}
